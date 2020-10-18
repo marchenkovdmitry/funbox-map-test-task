7d62da412a69f549d2c8d131a4e19684d49f6e42
marchenkovdmitry <marchenkovdmitry@gmail.com>
2020-10-18 21:42:16 +0300

    Updates

---
+++ precache-manifest.6732bc4d0f9dc774e75e73647d29d755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c5961e3da1199ef9609d1950a702b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecf906bce969b62d73d",
+    "url": "/static/css/main.5e2e3bd0.chunk.css"
+  },
+  {
+    "revision": "4432056523d45e04b8fe",
+    "url": "/static/js/2.066fc899.chunk.js"
+  },
+  {
+    "revision": "ee9d6ff43844efbe4ddeab3c8d2b69b8",
+    "url": "/static/js/2.066fc899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecf906bce969b62d73d",
+    "url": "/static/js/main.a1c56bd2.chunk.js"
+  },
+  {
+    "revision": "5ac509d26cbad7c74b14",
+    "url": "/static/js/runtime-main.6db3701c.js"
+  }
+]);
